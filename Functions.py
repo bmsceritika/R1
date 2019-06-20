c1bdf1179e8dcc1e6216427477c9363b3f4ef534
bmsceritika <51982170+bmsceritika@users.noreply.github.com>
2019-06-20 12:41:49 +0530

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,8 @@
+def data():
+	sname=input("Enter the servername")
+	dbname=input("Enter the Dbname")
+	uname=input("Enter your username")
+	passw=input("Enter your password")
+	a="servername="+sname+";"+"dbname="+dbname+";"+"userername="+uname+";"+"password="+passw
+	print(a)
+data()
